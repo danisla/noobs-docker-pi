2d9569663ff0c8074ac4d21003e7b595752cdb2d
Dan Isla <disla@jpl.nasa.gov>
2016-01-11 00:44:32 -0800

    fix to recipes that update config.txt

--- recipes/060-gpu-mem-512_INGREDIENTS/_RUNONCE/gpu_mem_512.sh
+++ recipes/060-gpu-mem-512_INGREDIENTS/_RUNONCE/gpu_mem_512.sh
@@ -6,4 +6,4 @@ grep -q "^gpu_mem=512" "$file" && exit
 
 echo "Adding gpu_mem=512 to $file" >> /dev/kmsg
 
-grep -q "^gpu_mem.*" "$file" &>/dev/null && sed -i -e 's/^gpu_mem.*/gpu_mem=512/' "$file" || echo "gpu_mem=512" >> "$file"
+grep -q "^gpu_mem.*" "$file" && sed -i -e 's/^gpu_mem.*/gpu_mem=512/' "$file" || echo "gpu_mem=512" >> "$file"
--- recipes/061-fast-usb-mouse_INGREDIENTS/_RUNONCE/fast_usb_mouse.sh
+++ recipes/061-fast-usb-mouse_INGREDIENTS/_RUNONCE/fast_usb_mouse.sh
@@ -6,4 +6,4 @@ grep -q "^usbhid.mousepoll=0" "$file" && exit
 
 echo "Adding usbhid.mousepoll=0 to $file" >> /dev/kmsg
 
-grep -q "^usbhid.mousepoll.*" "$file" &>/dev/null && sed -i -e 's/^usbhid.mousepoll.*/usbhid.mousepoll=0/' "$file" || echo "usbhid.mousepoll=0" >> "$file"
+grep -q "^usbhid.mousepoll.*" "$file" && sed -i -e 's/^usbhid.mousepoll.*/usbhid.mousepoll=0/' "$file" || echo "usbhid.mousepoll=0" >> "$file"
