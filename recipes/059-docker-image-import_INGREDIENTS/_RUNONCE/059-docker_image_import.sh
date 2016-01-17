62325e60d1c4e54a56d14d6286803e29b901e845
Dan Isla <disla@jpl.nasa.gov>
2016-01-17 12:13:40 -0800

    bugfix to size fetching

--- recipes/059-docker-image-import_INGREDIENTS/_RUNONCE/059-docker_image_import.sh
+++ recipes/059-docker-image-import_INGREDIENTS/_RUNONCE/059-docker_image_import.sh
@@ -3,7 +3,7 @@
 DOCKER_IMAGE_DIR="/home/pi/recovery/docker_images"
 
 for f in $(find "${DOCKER_IMAGE_DIR}" -type f -regextype posix-egrep -regex '.*(tar|gz|xz)$'); do
-    SIZE=$(du -sh noobs-docker-pi | awk '{print $1}')
+    SIZE=$(du -sh "${f}" | awk '{print $1}')
     echo "Import Docker image (${SIZE}) ${f}" >> /dev/kmsg
     docker load < "${f}"
 done
