b0c8945d07eaebd2c4e3ed288554c1f39c8d7527
Dan Isla <disla@jpl.nasa.gov>
2016-01-17 12:07:43 -0800

    update to docker image import messaging

--- recipes/059-docker-image-import_INGREDIENTS/_RUNONCE/059-docker_image_import.sh
+++ recipes/059-docker-image-import_INGREDIENTS/_RUNONCE/059-docker_image_import.sh
@@ -3,6 +3,7 @@
 DOCKER_IMAGE_DIR="/home/pi/recovery/docker_images"
 
 for f in $(find "${DOCKER_IMAGE_DIR}" -type f -regextype posix-egrep -regex '.*(tar|gz|xz)$'); do
-    echo "Import Docker image `du -sh ${f}`" >> /dev/kmsg
+    SIZE=$(du -sh noobs-docker-pi | awk '{print $1}')
+    echo "Import Docker image (${SIZE}) ${f}" >> /dev/kmsg
     docker load < "${f}"
 done
